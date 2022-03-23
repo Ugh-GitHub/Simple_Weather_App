8c5cdb75d9310ca990116346353bb0d4c520913a
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2022-03-22 21:41:27 -0500

    updated to remove a loop

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,29 +5,33 @@ import WeatherWrapper from '../WeatherWrapper/WeatherWrapper';
 
 function App() {
 
-  const [weatherIconsArray, setWeatherIconsArray] = useState([]);
-  const [dateTime] = useState(new Date());
+  const [weatherData, setWeatherData] = useState([]);
+  // const [weatherIconsArray, setWeatherIconsArray] = useState([]);
+  // const [dateTime] = useState(new Date());
 
   useEffect(() => {
     axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=44.9778&lon=-93.2650&exclude=current,minutely,hourly,alerts&units=imperial&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
     .then((response) => {
-      for (let i = 0; i < 5; i++) {
-        setWeatherIconsArray(oldArray => [...oldArray, {icon: response.data.daily[i].weather[0].icon, 
-          alt: response.data.daily[i].weather[0].description, 
-          min: response.data.daily[i].temp.min, 
-          max: response.data.daily[i].temp.max, 
-          weekday: (dateTime.getDay() + i)
-        }]);
-      }
+      setWeatherData(response.data.daily);
+      // for (let i = 0; i < 5; i++) {
+      //   setWeatherIconsArray(oldArray => [...oldArray, {icon: response.data.daily[i].weather[0].icon, 
+      //     alt: response.data.daily[i].weather[0].description, 
+      //     min: response.data.daily[i].temp.min, 
+      //     max: response.data.daily[i].temp.max, 
+      //     weekday: (dateTime.getDay() + i)
+      //   }]);
+      // }
     }).catch((error) => {
       console.log(error);
     });
-  },[dateTime])
+  },[])
+  // },[dateTime])
 
   return (
     <div className="App">
       <header className="App-header">
-        <WeatherWrapper icons={weatherIconsArray}/>
+        <WeatherWrapper data={weatherData}/>
+        {/* <WeatherWrapper icons={weatherIconsArray} */}
       </header>
     </div>
   );
--- src/Components/WeatherWrapper/WeatherWrapper.js
+++ src/Components/WeatherWrapper/WeatherWrapper.js
@@ -1,24 +1,38 @@
-function WeatherWrapper({ icons }) {
+import { useState } from 'react';
 
+function WeatherWrapper({ icons, data }) {
+    const [dateTime] = useState(new Date());
     const dayOfWeekName = (day) => {
         const dayOfWeekArray = ["Sun","Mon","Tues","Wed","Thurs","Fri","Sat"];
         return dayOfWeekArray[day];
       }
 
-    const images = icons.map((element, index) => {
+    const newImages = data.slice(0,5).map((element, index) => {
         return (
             <span className='weatherWrapper' id={index === 0 ? 'todaysWeatherItem' : ''} key={index}>
-                <span className='dayOfWeek'>{dayOfWeekName(element.weekday)}</span>
-                <img src={`http://openweathermap.org/img/wn/${element.icon}@2x.png`} key={index} alt={element.alt}/>
-                <span className='minTemp'>{Math.round(element.min)}&#176;</span>
-                <span className='maxTemp'>{Math.round(element.max)}&#176;</span>
-            </span>);
-       });
+                <span className='dayOfWeek'>{dayOfWeekName(dateTime.getDay() + index)}</span>
+                <img src={`http://openweathermap.org/img/wn/${element.weather[0].icon}@2x.png`} key={index} alt={element.weather[0].description}/>
+                <span className='minTemp'>{Math.round(element.temp.min)}&#176;</span>
+                <span className='maxTemp'>{Math.round(element.temp.max)}&#176;</span>
+            </span>
+    )});
+
+    // Tried to cut down on rerenders by using the original object array (instead of looping through to build a new array)
+    // const images = icons.map((element, index) => {
+    //     return (
+    //         <span className='weatherWrapper' id={index === 0 ? 'todaysWeatherItem' : ''} key={index}>
+    //             <span className='dayOfWeek'>{dayOfWeekName(element.weekday)}</span>
+    //             <img src={`http://openweathermap.org/img/wn/${element.icon}@2x.png`} key={index} alt={element.alt}/>
+    //             <span className='minTemp'>{Math.round(element.min)}&#176;</span>
+    //             <span className='maxTemp'>{Math.round(element.max)}&#176;</span>
+    //         </span>);
+    //    });
 
     return (
 
         <div className='container'>
-            {images}
+            {/* {images} */}
+            {newImages}
         </div>
     );
 }
